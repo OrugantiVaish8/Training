3be533c8338849fd216ebd169dad103d8e1e8efd
OrugantiVaish8 <orugantivaishnavi395@gmail.com>
2025-01-22 12:59:49 +0530

    FSD

--- DAY8/asyn.js
+++ DAY8/asyn.js
@@ -1,14 +1,23 @@
-// function main(m,n)
-// {
-//     setTimeout(()=>{
-//         for(let i=m; i<=n;i++ )
-//             {
-//                 console.log(i);
-//             }
-//     },5000)
-// }
-// // main(1,10)
-// main(1,"10a")
+function main(m,n)
+{
+    new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            if(isNaN(m)||isNaN(n)){
+                return reject()
+            }
+            for(let i=m;i<=n;i++){
+                console.log(i);
+            }
+            return resolve()
+        },5000)
+    })
+    .then(()=>console.log("Promise is resolved"))
+    .then(()=>console.log(10*10))
+    
+    .catch(()=>console.log("Error Occured"))
+}
+main(1,10)
+//main(1,"10a")
 // function demo()
 // {
 //     console.log("no's printed");
@@ -23,15 +32,15 @@
 //     console.log("i am set interval")
 // },3000)
 
-let online=window.navigator.online
-new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        if(online==true){
-            return resolve
-        }else{
-            reject
-        }
-    },5000)
-})
-.then(()=> console.log("online....."))
-.catch(()=> console.log("ofline....."))
+// let online=window.navigator.online
+// new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         if(online==true){
+//             return resolve()
+//         }else{
+//             return reject()
+//         }
+//     },5000)
+// })
+// .then(()=> console.log("online....."))
+// .catch(()=> console.log("offline....."))
