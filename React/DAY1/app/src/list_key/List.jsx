86035741f7111197182f7e291fd8392166e09641
OrugantiVaish8 <orugantivaishnavi395@gmail.com>
2025-02-08 13:00:34 +0530

    FSD

--- React/DAY1/app/src/list_key/List.jsx
+++ React/DAY1/app/src/list_key/List.jsx
@@ -1,50 +1,21 @@
-// import React, { Fragment } from 'react'
+import React, { Fragment } from 'react'
 
-// const List = () => {
-//     let arr=[10,20,30,40]
-//     console.log(arr);
-//     let num=0
-//     console.log(num);
-    
-//   return (
-//     <Fragment>
-//         {
-//             arr.map((x)=>{
-//                 console.log(x);
-//                 console.log(`<li key=${num++}>${x}</li>`);
-                
-//                 return(
-//                     <li key={num++}>{x}</li>
-//                 )
-//             })
-//         }
-//     </Fragment>
-//   )
-// }
-
-// export default List
- import React, { Fragment } from 'react'
- 
- const List = (props) => {
-    console.log(props.main);
-    
-    
+const List = (props) => {
+   console.log(props.main);
+   
    return (
      <Fragment>
+        <ol>
         {
-            props.main.map((x)=>{
-                return(
-                    <>
-                    <ol>
-                        <li key = {x.id}>{x.name}</li>
-                    </ol>
-                    </>
+            props.main.map((x, index) => {
+                return (
+                    <li key={x.id || index}>{x.name}</li> // Ensuring unique key
                 ) 
             })
         }
+        </ol>
      </Fragment>
    )
- }
- 
- export default List
- 
+}
+
+export default List;
