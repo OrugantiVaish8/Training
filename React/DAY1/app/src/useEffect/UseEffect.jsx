f18b5e28b5b55d1506dde11c315dbcc737592df0
OrugantiVaish8 <orugantivaishnavi395@gmail.com>
2025-02-13 13:01:15 +0530

    FSD

--- React/DAY1/app/src/useEffect/UseEffect.jsx
+++ React/DAY1/app/src/useEffect/UseEffect.jsx
@@ -1,61 +1,111 @@
 
-// //no dependency
-// import React, { useEffect ,useState} from 'react'
+// // //no dependency
+// // import React, { useEffect ,useState} from 'react'
+
+// // const UseEffect = () => {
+// //     let [state,setState]=useState(0)
+// //     useEffect(()=>{
+// //         setTimeout(()=>{
+// //             setState(()=>state+1)
+// //         },2000)
+// //     })
+// //   return (
+// //     <div>
+// //       <h1>{state}</h1>
+// //     </div>
+// //   )
+// // }
+
+// // export default UseEffect
+
+
+
+// //empty dependency
+// // import React, { useEffect ,useState} from 'react'
+
+// // const UseEffect = () => {
+// //     let [state,setState]=useState(0)
+// //     useEffect(()=>{
+// //         setTimeout(()=>{
+// //             setState(()=>state+1)
+// //         },2000)
+// //     },[])
+// //   return (
+// //     <div>
+// //       <h1>{state}</h1>
+// //     </div>
+// //   )
+// // }
+
+// // export default UseEffect
+
+// //dependency with value
+// import React, { useEffect, useState } from 'react'
 
 // const UseEffect = () => {
 //     let [state,setState]=useState(0)
-//     useEffect(()=>{
-//         setTimeout(()=>{
-//             setState(()=>state+1)
-//         },2000)
-//     })
+//     let [incr,setIncr]=useState(0)
+// useEffect(()=>{
+//     setIncr(()=>state*2)
+// },[state])
 //   return (
-//     <div>
-//       <h1>{state}</h1>
+//     <div>{state}
+//     <br />
+//     <button onClick={()=>setState((e)=>e+1)}>*</button>
+//     <br />
+//     {incr}
 //     </div>
 //   )
 // }
 
 // export default UseEffect
 
+//fetch api using useEffect use
+import React, { useEffect, useState } from "react";
+import "./UseEffect.css"; // Import CSS file
 
+const UserTable = () => {
+  const [users, setUsers] = useState([]);
 
-//empty dependency
-// import React, { useEffect ,useState} from 'react'
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=10")
+      .then((res) => res.json())
+      .then((data) => setUsers(data.results))
+      .catch((err) => console.error("Error fetching users:", err));
+  }, []);
 
-// const UseEffect = () => {
-//     let [state,setState]=useState(0)
-//     useEffect(()=>{
-//         setTimeout(()=>{
-//             setState(()=>state+1)
-//         },2000)
-//     },[])
-//   return (
-//     <div>
-//       <h1>{state}</h1>
-//     </div>
-//   )
-// }
-
-// export default UseEffect
-
-//dependency with value
-import React, { useEffect, useState } from 'react'
-
-const UseEffect = () => {
-    let [state,setState]=useState(0)
-    let [incr,setIncr]=useState(0)
-useEffect(()=>{
-    setIncr(()=>state*2)
-},[state])
   return (
-    <div>{state}
-    <br />
-    <button onClick={()=>setState((e)=>e+1)}>*</button>
-    <br />
-    {incr}
+    <div className="container">
+      <h2>User List</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Photo</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Country</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>
+                <img src={user.picture.thumbnail} alt="User" />
+              </td>
+              <td>{`${user.name.first} ${user.name.last}`}</td>
+              <td>{user.email}</td>
+              <td>{user.location.country}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default UseEffect
+export default UserTable;
+
+
+
