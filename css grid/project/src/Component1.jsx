6511a1878a982e649ac701b4b41ce614c151e464
OrugantiVaish8 <orugantivaishnavi395@gmail.com>
2025-02-25 12:57:11 +0530

    FSD

---
+++ css grid/project/src/Component1.jsx
@@ -0,0 +1,14 @@
+import './Component1.css';
+const Component1=()=>{
+   return(
+    <>
+        <div className="parent">
+            <div className="header">Header</div>
+            <div className="leftmenu">LeftMenu</div>
+            <div className="rightmenu">RightMenu</div>
+            <div className="footer">Footer</div>
+        </div>
+    </>
+   )
+}
+export default Component1;
